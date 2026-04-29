e9f39987f1e624380a4ece43220d2803c3167670
SergeyKudrin84 <conan4@yandex.ru>
2026-04-29 15:35:39 +1000

    Выполненное задание спринт 14

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -5,24 +5,6 @@ import org.mapstruct.MappingConstants;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.dto.UserDto;
 
-//public final class UserMapper{
-//    public static UserDto toUserDto(User user){
-//        UserDto userDto = new UserDto();
-//        userDto.setId(user.getId());
-//        userDto.setName(user.getName());
-//        userDto.setEmail(user.getEmail());
-//        return userDto;
-//    }
-//
-//    public static User toUser(UserDto userDto){
-//        User user = new User();
-//        user.setId(userDto.getId());
-//        user.setName(userDto.getName());
-//        user.setEmail(userDto.getEmail());
-//        return user;
-//    }
-//}
-
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
     UserDto toUserDto(User user); //map User to UserResponse
